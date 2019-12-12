3cb5b24d05b782fb9d1fbacdb8063fe48d065aca
Jeanne Choo <jchoo@aisingapore.org>
2019-12-12 14:44:58 +0800

    first cut of ventilator worker architecture

---
+++ README.md
@@ -0,0 +1,7 @@
+2019-12-09
+benchmark with REQ-REP architecture on XNLI test dataset 
+
+TODO:
+benchmark with ventilator worker concurrent socket architecture 
+
+some fun visualisations of multilingual embeddings 
--- worker.py
+++ worker.py
@@ -1,20 +1,23 @@
 import sys
 import time 
 import zmq
+from transformers import BertModel
+from utils import send_array, recv_array
+import torch
 
-context = zmq.Context()
-
-receiver = context.socket(zmq.PULL)
-receiver.connect("tcp://localhost:5557")
-
-sender = context.socket(zmq.PUSH)
-sender.connect("tcp://localhost:5558")
-
-while True:
-    s = receiver.recv()
-    
-    sys.stdout.write('.')
-    sys.stdout.flush()
-    
-    time.sleep(int(s)*0.001)
-    sender.send(b'')
+class Worker(model):
+    def __init__(self):
+        self.model = BertModel.from_pretrained(model).eval()
+    def run(self):
+        # setup zmq
+        context = zmq.Context()
+        # socket to receive work from ventilator
+        work_receiver = context.socket(zmq.PULL).connect("tcp://localhost:5557")
+        # socket to send results to sink
+        results_sender = context.socket(zmq.PUSH).connect("tcp://localhost:5558")
+        while True:
+            tokens_tensor = recv_array(work_receiver)
+            with torch.no_grad():
+                outputs = model(tokens_tensor)
+                encoded_layers = outputs[0].numpy()
+                send_array(results_sender, encoded_layers)
