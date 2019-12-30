e587392422b90f79b6f33c701f0f9bd501701aa5
Jeanne Choo <jchoo@aisingapore.org>
2019-12-30 15:31:13 +0800

    refactored cli app

--- mbert_server/install.sh
+++ mbert_server/install.sh
@@ -1,1 +1,1 @@
-pip3 install -e .
+pip install -e .
--- mbert_server/mbert_server/__main__.py
+++ mbert_server/mbert_server/__main__.py
@@ -1,9 +1,12 @@
 
-from mbert_server import MbertServer
-from utils import get_args_parser
-from multiprocessing import Process
+def main():
+    from mbert_server import MbertServer
+    from utils import get_args_parser
+    with MbertServer(get_args_parser()) as server:
+        server.join()
+
 
 if __name__=='__main__':
-    
+    main()
     
 
